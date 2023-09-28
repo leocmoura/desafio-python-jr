b143d8174fa8df26c2264bc7b54d25cc044dfa2f
Leonardo <leocordova96@gmail.com>
2023-09-28 13:46:52 -0300

    updade: serializer

--- xml_converter/serializer.py
+++ xml_converter/serializer.py
@@ -4,9 +4,10 @@ from xml_converter.converter import convert_to_dict
 
 class CreateXMLtoDictAPISerializer(serializers.Serializer):
     class Meta:
-        fields = 'file'
+        fields = ['file']
+        
     file = FileField()
-    converted_data = JSONField(required=False)
+    converted_data = JSONField(read_only=True)
 
     def create(self, validated_data):
         validated_data["converted_data"] = convert_to_dict(validated_data["file"])
